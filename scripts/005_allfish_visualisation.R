f2aaa5fd7c093974473a9d7b0c33f9c006a830fc
Sebastian Lopez-Marcano <slopezmarcano@gmail.com>
2023-04-03 08:32:56 +1000

    new fish added, csv changes and new graph

---
+++ scripts/005_allfish_visualisation.R
@@ -0,0 +1,21 @@
+#SLM
+#Created: 03042023
+
+#--DESCRIPTION--#
+#All the fish that I have tried in my life
+
+#--LIBRARY--#
+library(tidyverse)
+library(ggbump)
+library(ggthemes)
+library(ggtext) #add and modify text to ggpplot
+library(showtext) #fonts
+font_add_google("Lato")
+showtext_auto()
+
+#--VISUALISE RESULTS--#
+#FORMAT THE DATA IN THE GGBUMP FORMAT
+plot <- read_csv('data/fish_data.csv') %>%
+ggplot(aes(x = score, y = reorder(species, desc(score)))) + #TODO #6
+  geom_segment(aes(xend = 0, yend = species), color = "#ef476f") +
+  geom_point(size = 3, color = "#ef476f")
